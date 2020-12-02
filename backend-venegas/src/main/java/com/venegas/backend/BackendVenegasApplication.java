e37511c08347e2952e7c00323e7d4cd2c1cc1cd0
cavenegas2 <cavenegas2@espe.edu.ec>
2020-12-02 11:42:53 -0500

    proyecto creado

---
+++ backend-venegas/src/main/java/com/venegas/backend/BackendVenegasApplication.java
@@ -0,0 +1,13 @@
+package com.venegas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //define a la clase como un componente cuando se instancia
+public class BackendVenegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVenegasApplication.class, args);
+	}
+
+}
---
+++ backend-venegas/src/test/java/com/venegas/backend/BackendVenegasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venegas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVenegasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
